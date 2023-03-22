4d0e415e9c1a77ddda8a517f0ceec7217f2e97b5
somu3341 <somashekarvsv1996@gmail.com>
2023-03-22 18:28:39 +0530

    [somu]Add:Creating Stack Adding and Pushing.

---
+++ StacksAndQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace StacksAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag=true;
+            while(flag)
+            {
+                LinkedListStacks stacks= new LinkedListStacks();
+                Console.WriteLine("\n Choose Option to Perform \n1.Create Stack and Push \n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        stacks.Push(70);
+                        stacks.Push(30);
+                        stacks.Push(56);
+                        stacks.Dsiplay();
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+                }
+            }           
+        }
+    }
+}
